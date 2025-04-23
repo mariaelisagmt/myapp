1f054fadfb25a7af8afaa629ac182eb272b9419a
mariaelisagmt <mariaelisagmt@gmail.com>
2025-04-23 11:38:38 +0000

    initial project

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
