4b21109ac078517f3c135b0fc8517d9f2e848fc7
mariaelisagmt <mariaelisagmt@gmail.com>
2025-04-23 22:40:31 +0000

    FEAT: finish splash screen

--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -8,7 +8,46 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.backgroundColor,
-      body: Image.asset('assets/banners/banner_splash.png'),
+      body: Stack(
+        children: [
+          Image.asset('assets/banners/banner_splash.png'),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                spacing: 32.0,
+                children: [
+                  Image.asset('assets/logo.png', width: 192),
+                  Column(
+                    children: [
+                      Text(
+                        "Um parceiro inovador para sua",
+                        style: TextStyle(color: Colors.white, fontSize: 22.0),
+                      ),
+                      Text(
+                        "melhor experiência culinária!",
+                        style: TextStyle(
+                          color: AppColors.mainColor,
+                          fontSize: 22.0,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text("Bora!"),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
